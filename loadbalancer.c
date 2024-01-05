efdd648ab0240ab682b7fdd45d80ceb31db94ee0
Arian Sadeghi <arian.sadeghi@mail.utoronto.ca>
2024-01-05 12:46:25 -0500

    feat: Implemented initialization of inbound socket

--- loadbalancer.c
+++ loadbalancer.c
@@ -12,6 +12,8 @@
 #define MAX_SERVERS 10
 #define SERVERS_METADATA_PATH "./servers_metadata.txt"
 #define MAX_PATH_LEN 260
+#define LB_PORT 1800
+#define MAX_QUEUED_CONNECTIONS 100
 
 enum server_status {ACTIVE, INACTIVE, ERROR};
 
@@ -23,9 +25,33 @@ typedef struct server_details {
     enum server_status status;
     int num_connections;
     int max_connections;
+    pthread_mutex_t lock;
 } server_details;
 
 server_details **servers;
+
+
+/**
+ * @brief Initializes a new server_details structure and returns a pointer to it.
+ *
+ * @return Pointer to the initialized server_details structure.
+ */
+server_details *init_server() {
+    server_details *res = malloc(sizeof(server_details));
+    pthread_mutex_init(&res->lock, NULL);
+    return res;
+}
+
+/**
+ * @brief Deallocates the memory occupied by the server at the specified index.
+ *
+ * @param server_index Index of the server in the servers array.
+ */
+void deallocate_server(int server_index) {
+    pthread_mutex_destroy(&servers[server_index]->lock);
+    free(servers[server_index]);
+    servers[server_index] = NULL;
+}
 
 /**
  * @brief Loads server metadata stored in file specified by file_path. 
@@ -42,7 +68,7 @@ int load_servers_metadata(char *file_path) {
     char line[70];
     int count = 0;
     while (count < MAX_SERVERS && fgets(line, 70, file) != NULL) {
-        server_details *s = malloc(sizeof(server_details));
+        server_details *s = init_server();
         servers[count] = s;
         /* Assumes that the lines of the servers metadata file are formatted as follows:
             NAME ADDRESS PORT
@@ -75,17 +101,6 @@ void init_servers_metadata() {
 
 
 /**
- * @brief Deallocates the memory occupied by the server at the specified index.
- *
- * @param server_index Index of the server in the servers array.
- */
-void deallocate_server(int server_index) {
-    free(servers[server_index]);
-    servers[server_index] = NULL;
-}
-
-
-/**
  * @brief Establishes a connection to the server specified in the argument.
  *
  * Function is to be used as a thread routine.
@@ -94,6 +109,8 @@ void deallocate_server(int server_index) {
  */
 void *connect_to_server(void *arg) {
     server_details *server = (server_details *)arg;
+
+    pthread_mutex_lock(&server->lock);
 
     server->sockfd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_addr;
@@ -104,16 +121,18 @@ void *connect_to_server(void *arg) {
         if (temp == 0) fprintf(stderr, "Address not in acceptable format: %s\n", server->address);
         if (temp < 0) perror("inet_pton");
         server->status = ERROR;
-        return NULL;
+        goto cleanup_and_return;
     }
     
     if (connect(server->sockfd, (struct sockaddr *) &server_addr, sizeof server_addr) == -1) {
         perror("connect");
         server->status = ERROR;
-        return NULL;
+        goto cleanup_and_return;
     }
 
     server->status = ACTIVE;
+    cleanup_and_return:
+    pthread_mutex_unlock(&server->lock);
     return NULL;
 }
 
@@ -151,6 +170,38 @@ void connect_to_servers() {
 }
 
 
+/**
+ * @brief Initializes and configures an inbound socket for listening on the specified port.
+ *
+ * @param port Port number for the inbound socket.
+ * @return File descriptor of the initialized socket, or -1 if an error occurs.
+ */
+int init_inbound_socket(int port) {
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = INADDR_ANY;
+
+    int sock_fd;
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        return -1;
+    }
+
+    if (bind(sock_fd, (struct sockaddr *) &address, sizeof(address)) == -1) {
+        perror("bind");
+        return -1;
+    }
+
+    if (listen(sock_fd, MAX_QUEUED_CONNECTIONS) == -1) {
+        perror("listen");
+        return -1;
+    }
+
+    return sock_fd;
+}
+
+
 int main(int argc, char *argv[]) {
     servers = malloc(MAX_SERVERS * sizeof(server_details *));
     for (int i = 0; i < MAX_SERVERS; i++) servers[i] = NULL;
@@ -159,6 +210,13 @@ int main(int argc, char *argv[]) {
 
     connect_to_servers();
 
+    int inbound_sock_fd;
+    if ((inbound_sock_fd = init_inbound_socket(LB_PORT)) == -1) {
+        fprintf(stderr, "Failed to initialize inbound socket.\n");
+        goto cleanup_and_exit;
+    }
+
+    cleanup_and_exit:
     free(servers);
     return 0;
 }
